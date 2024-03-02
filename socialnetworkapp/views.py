abb96d8ce84c2c0f4252a528747cb9130ddf1c3b
Павел Берия <beriya2004@mail.ru>
2024-03-02 18:34:30 +0300

    Added CRUD requests for the User model

--- socialnetworkapp/views.py
+++ socialnetworkapp/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
 
 @api_view(['GET'])
 def getdata(request):
     return Response({"message": "Hello World"})
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
